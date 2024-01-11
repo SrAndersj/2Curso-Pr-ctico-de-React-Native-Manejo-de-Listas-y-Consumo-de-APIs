ffa95a6c92d8c78a8cf778cc82a19090b99a4da7
PcAudio <byandersj@gmail.com>
2024-01-11 15:45:56 -0500

    Consultando la API de detalles del Pokemon

--- pokedex/src/api/pokemon.js
+++ pokedex/src/api/pokemon.js
@@ -1,6 +1,4 @@
 import { API_HOST } from "../utils/constants";
-
-// peticion asincrona con async
 export async function getPokemonsApi(endpointUrl) {
   try {
     const url = `${API_HOST}/pokemon?limit=20&offset=0`;
@@ -11,9 +9,18 @@ export async function getPokemonsApi(endpointUrl) {
     throw error;
   }
 }
-
 export async function getPokemonDetailsByUrlApi(url) {
   try {
+    const response = await fetch(url);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    throw error;
+  }
+}
+export async function getPokemonDetailsApi(id) {
+  try {
+    const url = `${API_HOST}/pokemon/${id}`;
     const response = await fetch(url);
     const result = await response.json();
     return result;
--- pokedex/src/screens/Pokemon.js
+++ pokedex/src/screens/Pokemon.js
@@ -1,13 +1,31 @@
-import React, { Component } from "react";
-import { Text, SafeAreaView } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text } from "react-native";
+import { getPokemonDetailsApi } from "../api/pokemon";
 
 export default function Pokemon(props) {
-  const { navitagion, route } = props;
+  const {
+    navigation,
+    route: { params },
+  } = props;
+  const [pokemon, setPokemon] = useState(null);
 
-  console.log(route);
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await getPokemonDetailsApi(params.id);
+        setPokemon(response);
+      } catch (error) {
+        navigation.goBack();
+      }
+    })();
+  }, [params]);
+
+  if (!pokemon) return null;
+
   return (
-    <SafeAreaView>
-      <Text> estamos en un pokemon </Text>
-    </SafeAreaView>
+    <View>
+      <Text>Estamos en un POKEMON</Text>
+      <Text>{pokemon.name}</Text>
+    </View>
   );
 }
